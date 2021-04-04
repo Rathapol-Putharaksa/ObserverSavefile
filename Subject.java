b537ed51630c4b4aff5ee6309e1583ae6b6a550b
Rathapol-Putharaksa <61156321+Rathapol-Putharaksa@users.noreply.github.com>
2021-04-04 21:01:35 +0700

    Add files via upload

---
+++ Subject.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> file= new ArrayList<Observer>();
+ 
+  
+  public void addfile(Observer i){
+    file.add(i);    
+  }
+  public List getList(){return this.file;}
+ 
+   
+}
---
+++ googleAPI.java
@@ -0,0 +1,3 @@
+public class googleAPI extends Observer{
+    
+}
